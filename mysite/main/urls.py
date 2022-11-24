7c9a6e53585aea0970635850a6e2deaf439652af
hesdi <abdulaziz.gajnazarov@gmail.com>
2022-11-25 00:22:00 +0600

    shop_ready

--- mysite/main/urls.py
+++ mysite/main/urls.py
@@ -4,4 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('category/', views.category, name='category'),
+    path('cart/', views.cart, name='cart'),
+    path('confirmation/', views.confirmation, name='confirmation'),
+    path('checkout/', views.checkout, name='checkout'),
+    path('single-product/', views.single_product, name='single-product'),
 ]
--- mysite/main/views.py
+++ mysite/main/views.py
@@ -4,3 +4,23 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'aroma/index.html')
+
+
+def category(request):
+    return render(request, 'aroma/category.html')
+
+
+def cart(request):
+    return render(request, 'aroma/cart.html')
+
+
+def confirmation(request):
+    return render(request, 'aroma/confirmation.html')
+
+
+def checkout(request):
+    return render(request, 'aroma/checkout.html')
+
+
+def single_product(request):
+    return render(request, 'aroma/single-product.html')
